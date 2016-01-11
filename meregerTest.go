240e4a950d4f9697a6e9798791a6e23f4d71c5e4
s41nn0n <s41nn0n@gmail.com>
2016-01-11 08:40:26 +0200

    Adding Initial Unit Test

---
+++ meregerTest.go
@@ -0,0 +1,10 @@
+package merger
+
+const (
+	intoJSONStruct = "{\"UserID\":\"\",\"UserName\":\"959dfbb6-f143-468c-85e0-997b753f79a5@iprg.co.za\",\"Password\":\"\",\"Salt\":\"\",\"FirstName\":\"FN 97db7435-3706-4c85-a919-dc7b6845c39a\",\"LastName\":\"\",\"Email\":\"959dfbb6-f143-468c-85e0-997b753f79a5@iprg.co.za\",\"Kind\":\"user\",\"BirthDate\":\"2016-01-11T08:33:46.584+02:00\",\"Gender\":\"FN Male\",\"Verified\":[],\"AdditionalUserAttributes\":{\"UserPicLink\":\"\",\"Locale\":\"\",\"LocationLat\":\"\",\"LocationLot\":\"\"},\"ContactInformation\":[],\"UserType\":[],\"UserRoles\":[],\"OAuthData\":[]}"
+	fromJSONStruct = "{\"UserID\":\":19b90b42-786d-44bf-be74-6f6d54edfe02\",\"UserName\":\"959dfbb6-f143-468c-85e0-997b753f79a5@iprg.co.za\",\"Password\":\"\",\"Salt\":\"\",\"FirstName\":\"FN 97db7435-3706-4c85-a919-dc7b6845c39a\",\"LastName\":\"\",\"Email\":\"959dfbb6-f143-468c-85e0-997b753f79a5@iprg.co.za\",\"Kind\":\"user\",\"BirthDate\":\"2016-01-11T08:33:46.584+02:00\",\"Gender\":\"FN Male\",\"Verified\":[],\"AdditionalUserAttributes\":{\"UserPicLink\":\"\",\"Locale\":\"\",\"LocationLat\":\"\",\"LocationLot\":\"\"},\"ContactInformation\":[],\"UserType\":[],\"UserRoles\":[],\"OAuthData\":[]}"
+)
+
+// func init() {
+// 	json =
+// }
