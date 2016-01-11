5c9edbcf85a1b9eb81ad3f489289f54d7de002a3
s41nn0n <s41nn0n@gmail.com>
2016-01-11 11:23:13 +0200

    Test Fix

--- merger_test.go
+++ merger_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	intoJSONStructConst = "{\"UserID\":\"\",\"UserName\":\"959dfbb6-f143-468c-85e0-997b753f79a5@iprg.co.za\",\"Password\":\"\""
-	fromJSONStructConst = "{\"UserID\":\":19b90b42-786d-44bf-be74-6f6d54edfe02\",\"UserName\":\"959dfbb6-f143-468c-85e0-997b753f79a5@iprg.co.za\",\"Password\":\"\""
+	intoJSONStructConst = "{\"UserID\":\"\",\"UserName\":\"test@example.com\",\"Password\":\"PASS\"}"
+	fromJSONStructConst = "{\"UserID\":\"19b90b42-786d-44bf-be74-6f6d54edfe02\",\"UserName\":\"test@example.com\",\"Password\":\"\"}"
 )
 
 var intoJSONStruct, fromJSONStruct string
@@ -33,8 +33,44 @@ func TestMergeMasterJSON(t *testing.T) {
 	}
 
 	if x["UserID"] == nil && x["UserID"] == "" {
-		fmt.Println("UserID NILL !1!")
+		fmt.Println("UserID == NILL !1!")
 		t.FailNow()
 	}
 
+	if x["UserName"] != "test@example.com" {
+		fmt.Println("UserName Was changed")
+		t.FailNow()
+	}
+
+	if x["Password"] != "PASS" {
+		fmt.Println("Password Was Changed")
+		t.FailNow()
+	}
+
+	fmt.Println(x["UserID"])
+
+}
+
+func BenchmarkMergeMasterJSON(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var new string
+		var err error
+		intoJSONStruct = intoJSONStructConst
+		fromJSONStruct = fromJSONStructConst
+		if new, err = MergeMasterJSON(intoJSONStruct, fromJSONStruct); err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+
+		var x map[string]interface{}
+		if err = json.Unmarshal([]byte(new), &x); err != nil {
+			fmt.Println(err)
+			b.FailNow()
+		}
+
+		if x["UserID"] == nil && x["UserID"] == "" {
+			fmt.Println("UserID NILL !1!")
+			b.FailNow()
+		}
+	}
 }
